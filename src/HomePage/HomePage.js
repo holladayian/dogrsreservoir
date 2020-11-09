3cf4b2d864ea1702dec5dcdf52287f6b3c4d25b0
Ian Holladay <holladayian@gmail.com>
2020-11-08 16:17:08 -0900

    Install sass

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
+import './/HomePage.scss';
 
 export class HomePage extends Component {
   constructor(props) {
@@ -19,7 +20,7 @@ export class HomePage extends Component {
   render = () => {
     return (
       <div className="homepage">
-        <img src={this.state.newDogImg}></img>
+        <img src={this.state.newDogImg} className="dog-image"></img>
         <button className="random" onClick={this.setNewDog}>random</button>
         <button className="save">save</button>
       </div>
