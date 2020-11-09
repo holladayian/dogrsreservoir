92172749d705eb9324d8fff99fd48aebfce869f8
Ian Holladay <holladayian@gmail.com>
2020-11-09 10:20:00 -0900

    Add homepage test

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -29,7 +29,7 @@ export class HomePage extends Component {
           {this.props.currentDog ? <img src={this.props.currentDog} className="dog-image" data-testid='current-dog'></img> : ''}
         <div className='button-case'>
           <button className="random" onClick={this.setNewDog} data-testid='rand-btn'>random</button>
-          <button className="save" onClick={e => this.saveDog(e)} data-testid='save-btn'>favorite</button>
+          {this.props.currentDog ? <button className="save" onClick={e => this.saveDog(e)} data-testid='save-btn'>favorite</button> : ''}
         </div>
       </div>
     )
