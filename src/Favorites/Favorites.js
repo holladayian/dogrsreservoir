90ac3280c34d438b170802c125906289812bee8c
Ian Holladay <holladayian@gmail.com>
2020-11-08 22:05:22 -0900

    add key to favorites

--- src/Favorites/Favorites.js
+++ src/Favorites/Favorites.js
@@ -4,9 +4,8 @@ export const Favorites = (props) => {
 
 
   const showDogs = () => {
-    console.log(props.savedDogs)
     return props.savedDogs.map((dog, i) => {
-      return <img src={dog} className="dog-image"></img>
+      return <img src={dog} className="dog-image" key={i}></img>
     })
   }
 
