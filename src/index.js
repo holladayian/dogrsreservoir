81071355a6507a5a78ac36a607bcc7fbae5a469e
Ian Holladay <holladayian@gmail.com>
2020-11-08 19:15:15 -0900

    Install service worker

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Router } from 'react-router-dom';
-import History from './History.js';
+import * as serviceWorker from './serviceWorker';
+
+// import History from './History.js';
 
 // window.onload = History.push('/homepage')
 
 ReactDOM.render(
-  <Router history={History}>
+  <Router>
     <App />
   </Router>,
   document.getElementById('root')
@@ -18,4 +20,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.unregister();
