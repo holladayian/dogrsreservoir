70e9fe5cbf91f144993692e23e4c4c7523576525
Ian Holladay <holladayian@gmail.com>
2020-11-08 18:01:49 -0900

    Wrap app in router

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Router } from 'react-router-dom';
+import History from './History.js';
+
+// window.onload = History.push('/homepage')
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router history={History}>
     <App />
-  </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
 
@@ -15,5 +19,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-//initial commit
