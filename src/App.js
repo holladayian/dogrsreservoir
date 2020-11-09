889ae957fc6547ccb5a27c6c4919a9c589b57865
Ian Holladay (He/Him) <62005294+holladayian@users.noreply.github.com>
2020-11-08 19:33:39 -0900

    Revert "wrap homepage in route"
    This reverts commit 45d52c9427175e3aa950fa29c72defe6f0660791.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { HomePage } from './HomePage/HomePage.js';
 import { getNewDog } from './apiCalls/dogcalls.js';
-import { Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
@@ -36,15 +35,8 @@ class App extends Component {
           <h1>Dog Resivour</h1>
           <button className="favorites">Favorites</button>
         </header>
-        <Route
-          exact path='/homepage'
-          render={() => {
-            return (
-              <HomePage 
-              retrieveNewDog={this.retrieveNewDog}
-              />
-            )
-          }}
+        <HomePage 
+        retrieveNewDog={this.retrieveNewDog}
         />
       </div>
     );
