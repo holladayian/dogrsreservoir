8572c6d23eeea91c5e9391afb844d6fe5e24cd75
Ian Holladay (He/Him) <62005294+holladayian@users.noreply.github.com>
2020-11-24 19:29:59 -0900

    Add travi.yml file (#31)

--- src/App.test.js
+++ src/App.test.js
@@ -5,78 +5,58 @@ import App from './App';
 import { getNewDog } from './apiCalls/dogcalls';
 jest.mock('./apiCalls/dogcalls');
 
-
 describe('App', () => {
-  
-  it('1. should load a homepage with the `Favorites` button by default', () => {
-    
+  it('1. should load a homepage with the `Favorites` button by default', () => {   
       render(
         <BrowserRouter>
         <App />
       </BrowserRouter>
     );
-
     const appTest = screen.queryByTestId('dogs-reservoir');
-    expect(appTest).toBeInTheDocument();
-    
+    expect(appTest).toBeInTheDocument();  
     const home = screen.queryByTestId('homepage');
     expect(home).toBeInTheDocument();
-
     const faveButton = screen.queryByTestId('fav-btn');
-    expect(faveButton).toBeInTheDocument();
-    
-    
+    expect(faveButton).toBeInTheDocument();      
     // sad
     const faves = screen.queryByTestId('favorites');
     expect(faves).toEqual(null);
-
     const homeButton = screen.queryByTestId('home-btn');
     expect(homeButton).toEqual(null);
-  })
-  
-  it('2. should be able to switch between HomePage and Favorites', () => {
-    
+  }) 
+  it('2. should be able to switch between HomePage and Favorites', () => {   
       render(
         <BrowserRouter>
         <App />
       </BrowserRouter>
     );
-
     let faveButton = screen.queryByTestId('fav-btn');
     let favePage = screen.queryByTestId('favorites');
     let homeButton = screen.queryByTestId('home-btn');
-    let homePage = screen.queryByTestId('homepage');
-    
+    let homePage = screen.queryByTestId('homepage');  
     expect(faveButton).toBeInTheDocument();
     expect(favePage).toEqual(null);
     expect(homeButton).toEqual(null);
-    expect(homePage).toBeInTheDocument();
-    
-    userEvent.click(faveButton);
-    
+    expect(homePage).toBeInTheDocument();   
+    userEvent.click(faveButton);   
     faveButton = screen.queryByTestId('fav-btn');
     favePage = screen.queryByTestId('favorites');
     homeButton = screen.queryByTestId('home-btn');
-    homePage = screen.queryByTestId('homepage');
-    
+    homePage = screen.queryByTestId('homepage');  
     expect(faveButton).toEqual(null);
     expect(favePage).toBeInTheDocument();
     expect(homeButton).toBeInTheDocument();
-    expect(homePage).toEqual(null);
-    
-    userEvent.click(homeButton);
-    
+    expect(homePage).toEqual(null); 
+    userEvent.click(homeButton); 
     faveButton = screen.queryByTestId('fav-btn');
     favePage = screen.queryByTestId('favorites');
     homeButton = screen.queryByTestId('home-btn');
-    homePage = screen.queryByTestId('homepage');
-    
+    homePage = screen.queryByTestId('homepage');   
     expect(faveButton).toBeInTheDocument();
     expect(favePage).toEqual(null);
     expect(homeButton).toEqual(null);
     expect(homePage).toBeInTheDocument();
   })
-  
   it('3. should be able call getNewDog', async() => {
     getNewDog.mockResolvedValueOnce('newDog1')
     render(
@@ -84,17 +64,12 @@ describe('App', () => {
         <App />
       </BrowserRouter>
     );
-    
     let newDogButton = screen.queryByTestId('rand-btn');
     let noDog = screen.queryByTestId('current-dog');
     expect(noDog).toEqual(null);
-    
     userEvent.click(newDogButton);
-    
     let retriever = await waitFor(() => screen.getByTestId('current-dog'))
-    
     expect(retriever).toBeInTheDocument()
-    
   })
   
   it('4. should be able to favorite a picture of a dog in homepage, and see it in the favorites page', async() => {
